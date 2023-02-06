75307028306771a23e1dac7cbae94b22addd0a19
“kara <bouncingpie@icloud.com>
2023-02-05 23:31:55 -0500

    adding query text vars

--- helpers/queries.js
+++ helpers/queries.js
@@ -1,1 +1,8 @@
 import database from "../config/connection.js";
+
+// plain Text Query variables
+const getDepartments = "SELECT name FROM department";
+const getRoles = "SELECT title FROM role";
+const getManagers =
+  "SELECT first_name, last_name FROM employee WHERE manager_id is NULL";
+const getEmployees = "SELECT first_name, last_name FROM employee";
